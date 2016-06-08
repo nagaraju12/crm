34ad91f80839494f9e2204f08e44dba93e9ca1d1
nagaraju12 <kammaranagaraju@gmail.com>
2016-06-08 13:44:57 +0530

    add somethingd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,11 +76,11 @@ config.serve_static_assets = true
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-config.action_mailer.default_url_options = {:host => 'railscrms.herokuapp.com', :protocol => 'http'} #I've also tried it without ":protocol => 'http'"
- config.action_mailer.raise_delivery_errors = true
- config.action_mailer.delivery_method = :smtp
- config.action_mailer.perform_deliveries = true
- config.action_mailer.smtp_settings = {
+  config.action_mailer.default_url_options = {:host => 'railscrms.herokuapp.com', :protocol => 'http'} #I've also tried it without ":protocol => 'http'"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
      :address => "smtp.gmail.com",
      :port => 587,
      :authentication => :plain,   # I've also tried :login
