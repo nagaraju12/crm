e7886b7ae18ee3e479e7a3f1e17494b261a7aeb6
nagaraju12 <kammaranagaraju@gmail.com>
2016-06-09 13:19:30 +0530

    add heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,17 +55,17 @@ def change_password
 end
 
 def reset_password
-@user = User.find(params[:id])
-@password = params[:user][:password]
-@password_confirmation = params[:user][:password_confirmation]
-@user.errors.add(:password, "Is required") if (params[:user]).nil? or params[:user][:password].to_s.blank?
+ @user = User.find(params[:id])
+ @password = params[:user][:password]
+ @password_confirmation = params[:user][:password_confirmation]
+ @user.errors.add(:password, "Is required") if (params[:user]).nil? or params[:user][:password].to_s.blank?
 if @user.reset_password!(@password, @password_confirmation)
-sign_in(:user, @user ,:bypass => true)
-flash[:notice] = 'Password has reset successfully.'
-redirect_to users_path
+ sign_in(:user, @user ,:bypass => true)
+ flash[:notice] = 'Password has reset successfully.'
+ redirect_to users_path
 else
-render :action => 'change_password'
-end
+ render :action => 'change_password'
+  end
 end
 
 def change_email
